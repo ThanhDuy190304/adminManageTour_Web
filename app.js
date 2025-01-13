4d86df3750a9182337c4f4e792473af8661953e1
Lam Hong Anh Duc <neyduc167@gmail.com>
2025-01-14 02:50:29 +0700

    ddd

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.set('views', path.join(__dirname, 'src', 'views'))
 
 app.use(express.static(path.join(__dirname, 'src', 'public')));
 
-app.use('/', requireAdmin, viewsRoutes);
+app.use('/', viewsRoutes);
 app.use('/accountManagement', accountManagement);
 //app.use('/accountManagement', requireAdmin, accountManagement);
 //app.use('/reportByIncome', requireAdmin, reportRoutes);
